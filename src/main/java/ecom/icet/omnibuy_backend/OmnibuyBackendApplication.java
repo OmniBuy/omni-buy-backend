70d6c39d3b6adf11eeee15424ddd4e48556b118d
isura <isura3147@gmail.com>
2025-11-15 23:15:03 +0530

    inital commit

---
+++ src/main/java/ecom/icet/omnibuy_backend/OmnibuyBackendApplication.java
@@ -0,0 +1,13 @@
+package ecom.icet.omnibuy_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmnibuyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmnibuyBackendApplication.class, args);
+	}
+
+}
